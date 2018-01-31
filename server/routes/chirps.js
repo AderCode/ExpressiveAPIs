dcaed99c71c302d843e9f6e6c368886a03e6ce7c
Matthew Aderhold <matthewaderhold@gmail.com>
2018-01-31 11:42:05 -0600

    Step 5 complete (Required Lab Objectives Finished. For Advanced Objectives go to the `Advanced` branch on this repo)

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -17,26 +17,18 @@ router.post("/", (req, res) => {
   res.sendStatus(200);
 });
 
-router.put("/edit/id?", (req, res) => {
+router.put("/:id?", (req, res) => {
   //PUT stuff here lol xD :3 <(^_^<)
   let id = req.params.id;
-  if (id) {
   store.UpdateChirp(id, req.body);
   res.sendStatus(200);
-} else {
-    res.sendStatus(404);
-  }
 });
 
-router.delete("/delete/:id?", (req, res) => {
+router.delete("/:id?", (req, res) => {
   //DELETE stuff here Dx- - - - <(*_*<)
   let id = req.params.id;
-  if (id) {
     store.DeleteChirp(id);
     res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
-  }
 });
 
 module.exports = router;
